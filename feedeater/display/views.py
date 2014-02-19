201c79eba558c6f06d194719aadb85bc50f886b1
Jake Madison <jakemadison@gmail.com>
2014-02-19 08:48:20 -0800

    need to work out json to figure out how ajaxable to make this

--- feedeater/display/views.py
+++ feedeater/display/views.py
@@ -88,6 +88,115 @@ def return_a_page():
     login_form = LoginForm()
 
 
+@app.route('/json', methods=['GET', 'POST'])
+@app.route('/json/', methods=['GET', 'POST'])
+@app.route('/json/<int:page>', methods=['GET', 'POST'])
+@oid.loginhandler
+#@login_required
+def json_index(page=1):
+
+    print app
+    print __name__
+    user = None
+
+    user = g.user
+    form = LoginForm(request.form)
+    login_form = LoginForm()
+
+    add_feed_form = AddFeedForm(csrf_enabled=False)
+
+    if g.user.is_authenticated():
+        print '!!!!! - retrieving entries from is_authenticated call...'
+        entries = user.get_entries_new().paginate(page, c['POSTS_PER_PAGE'], False)
+
+    else:
+        print 'thiiiisssss..... is failing. move all this to user_man_feeds? ...actually, '
+        print 'sub_list returns a list of feedIds.. can just use those to get entries..'
+        entries = Entry.query.order_by(Entry.published.desc())[:10]
+        sub_list = user_manage_feeds.get_guest_feeds()
+        sl = sub_list['feed_data']
+        cats = []
+
+    if request.method == 'POST':
+        if not form.validate():
+            print 'maaaade it here:'
+            user = User(nickname="Guest", email="guest@guest.com", role=0)
+            return render_template("index.html", form=form, title='Home',
+                                   user=user, entries=entries, providers=app.config['OPENID_PROVIDERS'],
+                                   login_form=login_form, subs=sl)  # SL not defined yet...
+
+        if not user:
+            print 'made it here...'
+            form.errors = True
+            form.error_messages = ["The login details provided are not correct."]
+            return render_template("index.html", form=form, title='Home', user=user,
+                                   entries=entries, providers=app.config['OPENID_PROVIDERS'],
+                                   login_form=login_form)
+
+        print '!!!!yesssssssssssss!!!!'
+        login_user(user)
+        session['logged_in'] = True
+        flash("Logged in successfully.")
+
+    if login_form.validate_on_submit():
+        session['remember_me'] = login_form.remember_me.data
+        return oid.try_login(login_form.openid.data, ask_for=['nickname', 'email'])
+
+    if user is None or not g.user.is_authenticated():
+        print 'user is none!'
+        user = User(nickname="Guest", email="guest@guest.com", role=0)
+        sub_list = user_manage_feeds.get_guest_feeds()
+        cats = []
+
+    else:
+        print "}}}retrieving sublist here...."
+        sub_list = user_manage_feeds.get_user_feeds(user)
+        cats = sub_list['cat_list']
+        cats = sorted(cats)
+
+        print "================>", cats
+
+
+    print user
+
+    sl = sub_list['feed_data']  # sl needs to send count data as well. or send it from entries?
+
+    # print sl
+
+    test_entries = user.get_userentries().first()
+    print '------->', test_entries
+    # jsonify and paginate, easy as that!
+    # print dir(test_entries.remote_id)
+
+    print entries
+    print dir(entries)
+    json_entries = {}
+    for k, each in enumerate(entries.items):
+        print jsonify(each.json_entry())
+
+
+
+    print "\n"
+
+
+    # return jsonify({"subs": sl, "entries": entries})
+    return True
+    # with either json, or render, this should actually be returning the user_entry table joined with entry
+    # so we get a full list of user entries, tags, categories, stars, etc.
+
+    # return render_template("index.html", title='Home',
+    #                        user=user, entries=entries, form=form,
+    #                        providers=app.config['OPENID_PROVIDERS'],
+    #                        login_form=login_form, subs=sl, add_feed_form=add_feed_form,
+    #                        cats=cats)
+
+
+
+
+
+
+
+
 
 
 
