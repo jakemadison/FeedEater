e304c756ed8da5cd10c31f5a12f92dfc05e6c136
Jake Madison <jakemadison@gmail.com>
2014-06-17 16:01:13 -0700

    cleaning build_index in main.py

--- feedeater/display/views/main.py
+++ feedeater/display/views/main.py
@@ -13,7 +13,6 @@ from feedeater.controller import FeedGetter
 
 # This module should handle the main rendering & paging functions as well as login/out and authorization
 
-
 basedir = c.get('basedir')
 app = Blueprint('index', __name__, static_folder=basedir+'/display/static',
                 template_folder=basedir+'/display/templates')
@@ -25,10 +24,10 @@ def before_request():
     g.user = current_user
 
 
-
 @lm.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
 
 # @login_required
 @app.route('/', methods=['GET', 'POST'])
@@ -87,24 +86,6 @@ def f_login():
     return oid.try_login(url, ask_for=['nickname', 'email'])
 
 
-@app.route('/login', methods=['GET', 'POST'])
-@oid.loginhandler
-def login():
-
-    print 'running login now....'
-    if g.user is not None and g.user.is_authenticated():
-        return redirect(url_for('.build_index'))
-    form = LoginForm()
-
-    if form.validate_on_submit():
-        session['remember_me'] = form.remember_me.data
-        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
-    return render_template('login.html',
-                           title='Sign In',
-                           form=form,
-                           providers=flaskapp.config['OPENID_PROVIDERS'])
-
-
 @oid.after_login
 def after_login(resp):
 
@@ -116,7 +97,7 @@ def after_login(resp):
 
     user = User.query.filter_by(email=resp.email).first()
 
-    # if totally new user
+    # if totally new user:
     if user is None:
         print resp
         print dir(resp)
@@ -145,6 +126,8 @@ def logout():
 
 
 
+
+# this json hook crap needs to be moved to a different file.
 @app.route('/change_view', methods=['POST'])
 def change_view():
 
@@ -156,112 +139,3 @@ def change_view():
 
     return jsonify(success=True)
 
-
-
-@app.route('/_json', methods=['GET', 'POST'])
-def json_index(page=1):
-
-    # ok, i think i have a handle on this then. these view functions need to be
-    # divided. on one side, are all functions that return static/refresh data
-    # those need to use "render template". the other group needs to be json
-    # and those methods do not refresh, but do make server calls.
-
-    # print app
-    print __name__
-    user = None
-
-    user = g.user
-    form = LoginForm(request.form)
-    login_form = LoginForm()
-
-    add_feed_form = AddFeedForm(csrf_enabled=False)
-
-    if g.user.is_authenticated():
-        print '!!!!! - retrieving entries from is_authenticated call...'
-        entries = user.get_entries_new().paginate(page, c['POSTS_PER_PAGE'], False)
-
-    else:
-        print 'thiiiisssss..... is failing. move all this to user_man_feeds? ...actually, '
-        print 'sub_list returns a list of feedIds.. can just use those to get entries..'
-        entries = Entry.query.order_by(Entry.published.desc())[:10]
-        sub_list = user_manage_feeds.get_guest_feeds()
-        sl = sub_list['feed_data']
-        cats = []
-
-    if request.method == 'POST':
-        if not form.validate():
-            print 'maaaade it here:'
-            user = User(nickname="Guest", email="guest@guest.com", role=0)
-            return render_template("index.html", form=form, title='Home',
-                                   user=user, entries=entries, providers=flaskapp.config['OPENID_PROVIDERS'],
-                                   login_form=login_form, subs=sl)  # SL not defined yet...
-
-        if not user:
-            print 'made it here...'
-            form.errors = True
-            form.error_messages = ["The login details provided are not correct."]
-            return render_template("index.html", form=form, title='Home', user=user,
-                                   entries=entries, providers=flaskapp.config['OPENID_PROVIDERS'],
-                                   login_form=login_form)
-
-        print '!!!!yesssssssssssss!!!!'
-        login_user(user)
-        session['logged_in'] = True
-        flash("Logged in successfully.")
-
-    if login_form.validate_on_submit():
-        session['remember_me'] = login_form.remember_me.data
-        return oid.try_login(login_form.openid.data, ask_for=['nickname', 'email'])
-
-    if user is None or not g.user.is_authenticated():
-        print 'user is none!'
-        user = User(nickname="Guest", email="guest@guest.com", role=0)
-        sub_list = user_manage_feeds.get_guest_feeds()
-        cats = []
-
-    else:
-        print "}}}retrieving sublist here...."
-        sub_list = user_manage_feeds.get_user_feeds(user)
-        cats = sub_list['cat_list']
-        cats = sorted(cats)
-
-        print "================>", cats
-
-
-    print user
-
-    sl = sub_list['feed_data']  # sl needs to send count data as well. or send it from entries?
-
-    # print sl
-
-    test_entries = user.get_userentries().first()
-    print '------->', test_entries
-    # jsonify and paginate, easy as that!
-    # print dir(test_entries.remote_id)
-
-    print entries
-    print dir(entries)
-    json_entries = {}
-    for k, each in enumerate(entries.items):
-        print jsonify(each.json_entry())
-
-
-
-    print "\n"
-
-
-    # return jsonify({"subs": sl, "entries": entries})
-    return jsonify({"subs": sl})
-    # with either json, or render, this should actually be returning the user_entry table joined with entry
-    # so we get a full list of user entries, tags, categories, stars, etc.
-
-    # return render_template("index.html", title='Home',
-    #                        user=user, entries=entries, form=form,
-    #                        providers=app.config['OPENID_PROVIDERS'],
-    #                        login_form=login_form, subs=sl, add_feed_form=add_feed_form,
-    #                        cats=cats)
-
-
-@app.route('/json.html')
-def json_index():
-    return render_template("jsonindex.html")
