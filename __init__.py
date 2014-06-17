fc80be5b4c3cdf8375517ae9369c9dc204e41d06
Jake Madison <jakemadison@gmail.com>
2014-06-16 18:34:18 -0700

    minor (much needed) file cleanups

--- __init__.py
+++ __init__.py
@@ -5,6 +5,3 @@ from sys import path
 
 path.append(dirname(dirname(__file__)))
 
-# TODO: might be a good idea to incorporate a webpage craweler to attempt to find
-# RSS links within the page.  could return multiple results and ask user to pick
-# between them.
