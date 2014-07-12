65fbd9f36bef737fb9da008be837ba4c1866b11e
Jake Madison <jakemadison@gmail.com>
2014-07-12 14:46:01 -0700

    wow, finally got somewhere with this reading directive.

--- feedeater/display/static/js/customDirectives.js
+++ feedeater/display/static/js/customDirectives.js
@@ -7,6 +7,8 @@ customDirectives.directive('keyBindings', function() {
         restrict: "A",
         link: function(scope, element, attrs) {
 
+
+
             console.log('called outside', element);
             element.bind("keyup", function(event){
 
@@ -20,7 +22,7 @@ customDirectives.directive('keyBindings', function() {
                     switch (event.which) {
 
                         case 37:
-                            console.log('left was pressed');
+                            console.log('left was pressedddd');
                             scope.$broadcast('keypress', 'left');
                             event.preventDefault();
                             break;
@@ -36,3 +38,100 @@ customDirectives.directive('keyBindings', function() {
         }
     }
 });
+
+
+customDirectives.directive('scrollActive', function($window, $document) {
+
+    return {
+        require: '^?uiScrollfixTarget',
+        restrict: 'A',
+        link: function(scope, elm, attrs, uiScrollfixTarget) {
+
+            var top = elm[0].offsetTop,
+                $target = uiScrollfixTarget && uiScrollfixTarget.$element || angular.element($window);
+
+
+            function onScroll() {
+
+                var offset = $window.pageYOffset;
+                console.log('offset: ', offset);
+
+                if (!elm.hasClass('reading_entry') && offset > 100) {
+                    elm.addClass('reading_entry');
+                }
+
+                else if (elm.hasClass('reading_entry') && offset < 100) {
+                    elm.removeClass('reading_entry');
+                 }
+            }
+
+            $target.on('scroll', onScroll);
+
+
+            //raw.scrollTop + raw.offsetHeight >= raw.scrollHeight
+
+//          element.on("mouseenter", function() {
+//              console.log('mouseenter');
+//          });
+
+    }
+};
+
+});
+
+
+customDirectives.directive('uiScrollfixTarget', function(){
+   return {
+        controller: ['$element', function($element) {
+            this.$element = $element;
+        }]
+    };
+});
+
+
+
+//    return {
+//        restrict: 'E',
+//        transclude: true,
+//        link: function(scope, element, attrs) {
+//            console.log('anything?');
+//
+//            var window = angular.element($window),
+//                parent = angular.element(element.parent()),
+//                currentOffsetTop = element.offset().top;
+//
+//            handleScroll();
+//
+//            console.log('scroll directive: ', element, attrs);
+//
+//            window.bind("scroll", function() {
+//                console.log('bind scroll works');
+//               handleScroll();
+//            });
+//
+//            element.bind("mouseenter", function() {
+//               console.log('mouseenter happened');
+//            });
+//
+//
+//
+//            function handleScroll() {
+//                console.log('handleScroll');
+//                if (window.scrollTop() > currentOffsetTop) {
+//                    console.log('something happened: ', currentOffsetTop, parent, attrs);
+//                }
+//                else {
+//                    console.log('else..');
+//                }
+//
+//            }
+//
+//
+//
+//
+//        }
+//
+//    };
+
+
+//});
