258892346d995c79361873cae1fa487eccefb28e
Jake Madison <jakemadison@gmail.com>
2014-09-16 16:22:46 -0700

    nothing really again.  just a couple import cleans.

--- feedeater/config.py
+++ feedeater/config.py
@@ -4,8 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-#from flask.ext.sqlalchemy import SQLAlchemy
-#from flask import Flask
+
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database/app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'database/db_repository')
